4098cf131b71260be5b4b5249b7428b7b1724cb2
jarvistyro <64657492+jarvistyro@users.noreply.github.com>
2020-05-26 09:47:15 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from keras.optimizers import Adam
+from keras.layers import Dense
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv('/code/wines.csv')
+
+y = dataset['Class']
+y_cat = pd.get_dummies(y)
+x = dataset.drop('Class', axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y_cat,test_size=0.1,random_state=20)
+
+model = Sequential()
+model.add(Dense(units=64, input_shape=(13,), activation='relu'))
+model.add(Dense(units=3, activation='softmax'))
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=Adam())
+model.fit(x_train,y_train, validation_data=(x_test,y_test), epochs=20, verbose=0)
+
+accuracy = model.evaluate(x_test, y_test, verbose=0)
+accuracy = accuracy[1]*100
+
+import os
+os.system("sudo touch /code/accuracy.txt")
+os.system("echo {} > /code/accuracy.txt".format(accuracy))
+
+model.save('/code/multiclassNN.h5')
